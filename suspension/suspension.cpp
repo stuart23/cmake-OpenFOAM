58d1269318637e56414dac629dbda044887aa8d9
Stuart Buckingham <stuart.buckingham@gmail.com>
2017-02-23 21:30:02 +0000

    simple program that creates a double wishbone suspension system

--- suspension/suspension.cpp
+++ suspension/suspension.cpp
@@ -9,16 +9,24 @@ int main() {
 	// Forces in the system (not required)
 	GeneralForceSubsystem forces(system);
 	// Gravity (not required apparently)
-	Force::UniformGravity gravity(forces, matter, Vec3(0, -9.8, 0));
+	Force::UniformGravity gravity(forces, matter, Vec3(0, 0, -9.8));
 	// Mass = 1kg, CoG at 0,0,0 and 1kgm^2 rotational inertia in all 3 axis
-	Body::Rigid pendulumBody(MassProperties(1.0, Vec3(0), Inertia(1)));
-	// Adding the joints. The joint is a pin joint between ground (first arg) and an INSTANCE
-	// of pendulumBody (therefore used in both lines)
-	// 2nd and 4th args define joint position and orientation
-	MobilizedBody::Pin pendulum1(matter.Ground(), Transform(Vec3(0)),
-			pendulumBody, Transform(Vec3(0,1,0)));
-	MobilizedBody::Pin pendulum2(pendulum1, Transform(Vec3(0)),
-			pendulumBody, Transform(Vec3(0,1,0)));
+	Body::Rigid suspension(MassProperties(1.0, Vec3(0), Inertia(1)));
+	
+	MobilizedBody::Free top_wishbone(matter.Ground(), Transform(Vec3(0)), suspension, Transform(Vec3(0)));
+
+	Constraint::Ball top_fore_spherical(matter.Ground(), Vec3(0.1,0,0.5), top_wishbone, Vec3(0.1,0,0.5));
+	Constraint::Ball top_aft_spherical(matter.Ground(), Vec3(-0.1,0,0.5), top_wishbone, Vec3(-0.1,0,0.5));
+
+	MobilizedBody::Free bottom_wishbone(matter.Ground(), Transform(Vec3(0)), suspension, Transform(Vec3(0)));
+
+	Constraint::Ball bottom_fore_spherical(matter.Ground(), Vec3(0.1,0,0.3), bottom_wishbone, Vec3(0.1,0,0.3));
+	Constraint::Ball bottom_aft_spherical(matter.Ground(), Vec3(-0.1,0,0.3), bottom_wishbone, Vec3(-0.1,0,0.3));
+
+	MobilizedBody::Free upright(matter.Ground(), Transform(Vec3(0)), suspension, Transform(Vec3(0)));
+
+	Constraint::Ball top_ob_spherical(bottom_wishbone, Vec3(0.0,0.5,0.5), upright, Vec3(0.0,0.5,0.5));
+	Constraint::Ball bottom_ob_spherical(top_wishbone, Vec3(-0.0,0.5,0.3), upright, Vec3(-0.0,0.5,0.3));
 
 	// Purely for visualisation (optional)
 	Visualizer viz(system);
@@ -27,7 +35,6 @@ int main() {
 	// Finalizes the geometry
 	system.realizeTopology();
 	State state = system.getDefaultState();
-	pendulum2.setRate(state, 5.0);
 
 
 	RungeKuttaMersonIntegrator integ(system);
