eb4729ef5f4a3bb8cdbe3edae3496afbe3a1dcdb
Stuart Buckingham <stuart.buckingham@gmail.com>
2017-02-23 02:28:55 +0000

    not working

--- python/suspension/__init__.py
+++ python/suspension/__init__.py
@@ -1,27 +1,37 @@
 from primitives import Point
 
-def Member(**kwargs):
+class SuspensionMember(object):
     """
-    Class factory that returns the appropriate class depending
-    on the arguments used.
+    Super class inherited by Suspension Member classes. Do not instantiate
+    directly.
     """
-    wishbone_properties = ['ob_point', 'ib_fore_point', 'ib_aft_point']
-    rod_properties = ['ob_point', 'ib_point']
+    def __init__(self, parent, **kwargs):
+        for arg in kwargs.iteritems():
+            if arg[0] not in self._properties.keys():
+                raise ValueError('Valid keyword arguments for this class are: \
+                        %s' % ', '.join(self._properties))
+            expected_type = self._properties[arg[0]]
+            if not isinstance(args[1], expected_type): 
+                raise TypeError('Value of %s expected to be %s' % 
+                        (args[1], expected_type))
+            self.__setattr__(arg[0], arg[1])
 
-    class SuspensionMember(object):
-        def __init__(self, **kwargs):
-            for key, value in kwargs:
-                self.__setattr__(key, value)
-    class Wishbone(SuspensionMember):
-        pass
-    class Rod(SuspensionMember):
-        pass
+class Wishbone(SuspensionMember):
+    def __init__(self, **kwargs):
+        self._properties = {
+                'ob_point': Point,
+                'ib_fore_point': Point, 
+                'ib_aft_point': Point
+                }
+        super(Wishbone, self).__init__(kwargs)
 
-    if all([props in kwargs.keys() for props in wishbone_properties]):
-        print 'wishbone'
-        return Wishbone(kwargs)
-    elif all([props in kwargs.keys() for props in rod_properties]):
-        return Rod(kwargs)
+class Rod(SuspensionMember):
+    def __init__(self, **kwargs):
+        self._properties = {
+                'ob_point': Point,
+                'ib_point': Point
+                }
+        super(Rod, self).__init__(kwargs)
 
 class Suspension(object):
     def __init__(self):
