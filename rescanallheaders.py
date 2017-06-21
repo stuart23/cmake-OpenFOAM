085ff505c0eb9723697ca7346af2010246525bf2
Stuart Buckingham <stuart.buckingham@gmail.com>
2017-06-21 18:05:10 +0100

    created recursive cmakelist filler

---
+++ rescanallheaders.py
@@ -0,0 +1,85 @@
+#!/bin/python
+import os
+
+def findheaders(cmake_file_name):
+    started = False
+    source_files = []
+    with open(cmake_file_name) as cmake_file:
+        for line in cmake_file.readlines():
+            if line.find('SOURCES') > 0:
+                started = True
+            elif started and (line.find(')') > -1):
+                break
+            elif started:
+                source_files.append(line.strip())
+    
+    source_files = filter(lambda x: len(x) > 1, source_files)
+    
+    def readIncludes(filename):
+        include_files = []
+        with open(filename) as source_file:
+            for line in source_file.readlines():
+                if (line.find('#') >= 0) and (line.find('include') > 0):
+                    header_file = filter(lambda a: a.find('.H') > 0 or a.find('.h') > 0 or a.find('.C') > 0 , line.split())
+                    include_files += header_file
+            include_files = [a.replace('"','') for a in include_files]
+        return include_files
+    
+    def findFile(filename):
+        for root, directory, files in os.walk(os.getcwd()):
+            if filename in files:
+                return('include_directories( %s )' % root.replace('/home/stuart/cmake-OpenFOAM','${CMAKE_SOURCE_DIR}'),
+                    os.path.join(root, filename))
+        for root, directory, files in os.walk('/home/stuart/cmake-OpenFOAM/src'):
+            if filename in files:
+                return('include_directories( %s )' % root.replace('/home/stuart/cmake-OpenFOAM','${CMAKE_SOURCE_DIR}'),
+                    os.path.join(root, filename))
+        for root, directory, files in os.walk(os.path.abspath(os.path.join(os.getcwd(),'..'))):
+            if filename in files:
+                return('include_directories( %s )' % root.replace('/home/stuart/cmake-OpenFOAM','${CMAKE_SOURCE_DIR}'),
+                    os.path.join(root, filename))
+        for root, directory, files in os.walk('/home/stuart/cmake-OpenFOAM/applications'):
+            if filename in files:
+                return('include_directories( %s )' % root.replace('/home/stuart/cmake-OpenFOAM','${CMAKE_SOURCE_DIR}'),
+                    os.path.join(root, filename))
+    
+    def walkFile(filename, scanned_files, depth):
+        includes = readIncludes(filename)
+        directories = []
+        if len(includes) == 0:
+            return ['', scanned_files]
+        else:
+            for filename in includes:
+                if filename not in scanned_files:
+                    scanned_files.append(filename)
+                    paths = findFile(filename)
+                    if paths:
+                        #print( '. '*depth + 'FOUND %s' % paths[1])
+                        directories.append(paths[0])
+                        [ new_directories, scanned_files ] = walkFile(paths[1], scanned_files, depth + 1)
+                        directories += new_directories
+                #else:
+                    #print '. '*depth + "%s ALREADY SCANNED %i files" % ( filename, len(scanned_files))
+            return [filter(lambda x: len(x) > 1, directories), scanned_files]
+    
+    all_includes = []
+    scanned_files = []
+    for filename in source_files:
+        [new_includes, scanned_files] = walkFile(filename, scanned_files, 0)
+        all_includes += new_includes
+    unique_includes = list(set(all_includes))
+    unique_includes.sort()
+
+    old_lines = []
+    with open(cmake_file_name) as cmake_file:
+        old_lines = cmake_file.readlines()
+
+    with open(cmake_file_name, "w") as cmake_file:
+        for line in old_lines:
+            if line.startswith("include_directories( ${CMAKE_SOURCE_DIR}/"):
+                break
+            cmake_file.write(line)
+
+        cmake_file.write('\n'.join(unique_includes))
+
+findheaders("CMakeLists.txt")
